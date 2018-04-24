c4fb3089b32a0fed1577b03004c712f493f602d0
Lucas Ghibert <lghibert@emi.u-bordeaux1.fr>
2018-04-24 16:36:19 +0200

    Drag and drop chelou mais rigolo

--- gui/FXView.java
+++ gui/FXView.java
@@ -1,30 +1,46 @@
 package gui;
 
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class FXView implements View {
 	
 	public static Scene scene;
 	public static BorderPane pane;
+	public static Group group;
 	
 	public static void drawFrame(Stage stage) {
 		stage.setTitle("Projet AL");
 		//controller = Controller.getInstance();
 		
 		pane = new BorderPane();
-		scene = new Scene(pane, 600, 600);
+		
 		
 		pane.setTop(drawCommandBar());
 		pane.setLeft(drawToolBar());
 		
-		//pane.getChildren().add(drawCommandBar());
-		//pane.getChildren().add(drawToolBar());
+		Rectangle r = new Rectangle(200, 50, Color.BLACK);
+		Rectangle r2LeRetour = new Rectangle(100, 100, Color.BLUE);
+		group = new Group(r, r2LeRetour);
+		pane.setCenter(group);
+		//TranslateTransition transition = createTranslateTransition(r);
+		
+		scene = new Scene(pane, 600, 600);
+	    //moveOnMousePress(scene, r, transition);
 		
 		/* 
 		startGame = new Button("Start");
@@ -39,6 +55,93 @@ public class FXView implements View {
 		easy.relocate(110, 35);
 		pane.getChildren().add(easy);
 		*/
+		
+		r.setOnDragDetected(new EventHandler <MouseEvent>() {
+            public void handle(MouseEvent event) {
+                /* drag was detected, start drag-and-drop gesture*/
+                System.out.println("onDragDetected");
+                
+                /* allow any transfer mode */
+                Dragboard db = r.startDragAndDrop(TransferMode.ANY);
+                
+                /* put a string on dragboard */
+                ClipboardContent content = new ClipboardContent();
+                content.putString("Cotelettes");
+                db.setContent(content);
+                
+                event.consume();
+            }
+        });
+
+        r2LeRetour.setOnDragOver(new EventHandler <DragEvent>() {
+            public void handle(DragEvent event) {
+                /* data is dragged over the target */
+                System.out.println("onDragOver");
+                
+                /* accept it only if it is  not dragged from the same node 
+                 * and if it has a string data */
+                if (event.getGestureSource() != r2LeRetour) {
+                    /* allow for both copying and moving, whatever user chooses */
+                    event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+                }
+                
+                event.consume();
+            }
+        });
+
+        r2LeRetour.setOnDragEntered(new EventHandler <DragEvent>() {
+            public void handle(DragEvent event) {
+                /* the drag-and-drop gesture entered the target */
+                System.out.println("onDragEntered");
+                /* show to the user that it is an actual gesture target */
+                if (event.getGestureSource() != r2LeRetour) {
+                    r2LeRetour.setFill(Color.GREEN);
+                }
+                
+                event.consume();
+            }
+        });
+
+        r2LeRetour.setOnDragExited(new EventHandler <DragEvent>() {
+            public void handle(DragEvent event) {
+                /* mouse moved away, remove the graphical cues */
+                r2LeRetour.setFill(Color.RED);
+                
+                event.consume();
+            }
+        });
+        
+        r2LeRetour.setOnDragDropped(new EventHandler <DragEvent>() {
+            public void handle(DragEvent event) {
+                /* data dropped */
+                System.out.println("onDragDropped");
+                /* if there is a string data on dragboard, read it and use it */
+                Dragboard db = event.getDragboard();
+                boolean success = false;
+                if (db.hasString()) {
+                    r2LeRetour.setFill(Color.TURQUOISE);
+                    success = true;
+                }
+                /* let the source know whether the string was successfully 
+                 * transferred and used */
+                event.setDropCompleted(success);
+                
+                event.consume();
+            }
+        });
+
+        r.setOnDragDone(new EventHandler <DragEvent>() {
+            public void handle(DragEvent event) {
+                /* the drag-and-drop gesture ended */
+                System.out.println("onDragDone");
+                /* if the data was successfully moved, clear it */
+                if (event.getTransferMode() == TransferMode.MOVE) {
+                    r.setFill(Color.BISQUE);
+                }
+                
+                event.consume();
+            }
+        });
 		
 		stage.setScene(scene);
 		stage.show();
@@ -92,4 +195,33 @@ public class FXView implements View {
 	    
 	    return vbox;
 	}
+    
+
+  /*private static TranslateTransition createTranslateTransition(Rectangle r) {
+    final TranslateTransition transition = new TranslateTransition(Duration.seconds(0.25), r);
+    transition.setOnFinished(new EventHandler<ActionEvent>() {
+      @Override public void handle(ActionEvent t) {
+        r.setX(r.getTranslateX() + r.getX());
+        r.setY(r.getTranslateY() + r.getY());
+        r.setTranslateX(0);
+        r.setTranslateY(0);
+      }
+    });
+    return transition;
+  }
+  
+  private static void moveOnMousePress(Scene scene, Rectangle r, TranslateTransition transition) {
+    scene.setOnMousePressed(new EventHandler<MouseEvent>() {
+      @Override public void handle(MouseEvent event) {
+        if (!event.isControlDown()) {
+          r.setX(event.getSceneX());
+          r.setY(event.getSceneY());
+        } else {
+          transition.setToX(event.getSceneX() - r.getX());
+          transition.setToY(event.getSceneY() - r.getY());
+          transition.playFromStart();
+        }  
+      }
+    });
+  }*/
 }
